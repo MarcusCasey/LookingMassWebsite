e434242eaa94117be7011fcf71b3f55e97cd0128
Marcus <marcus.casey@outlook.com>
2019-08-29 13:06:06 -0700

    deployed to firebase and working

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "../../core/services/user.service";
 
 @Component({
-  selector: 'app-user-list',
-  templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
+  selector: "app-user-list",
+  templateUrl: "./user-list.component.html",
+  styleUrls: ["./user-list.component.scss"]
 })
 export class UserListComponent implements OnInit {
+  users;
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit() {
+    this.users = this.userService.getUsers();
   }
-
 }
